25ad0e21700cfa552f1f4891fb9fed77e18378b6
faisalwidyono <fwidyono@gmail.com>
2019-08-19 11:43:41 +0700

    first commit

---
+++ src/main/java/com/faisal/dealertrial/DealertrialApplication.java
@@ -0,0 +1,13 @@
+package com.faisal.dealertrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealertrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealertrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faisal/dealertrial/DealertrialApplicationTests.java
@@ -0,0 +1,16 @@
+package com.faisal.dealertrial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DealertrialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
